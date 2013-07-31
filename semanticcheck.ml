45fdf089a87e77be46466d3c01d7cb556d91b0f0
Hung Do <hungdohuy@gmail.com>
2013-08-01 00:50:46 +0700

    52/122

--- semanticcheck.ml
+++ semanticcheck.ml
@@ -18,15 +18,15 @@ exception NeverHappen
 
 (* return a type of a constant *)
 let get_const_type c =
-match c with
-IntVal _ -> TypeInt
-| FloatVal _ -> TypeFloat
-| StringVal _ -> TypeString
-| BoolVal _ -> TypeBool
-| Null -> TypeClass "?null"
+	match c with
+	IntVal _ -> TypeInt
+	| FloatVal _ -> TypeFloat
+	| StringVal _ -> TypeString
+	| BoolVal _ -> TypeBool
+	| Null -> TypeClass "?null"
 
 (* invoked when entering a scope *)
-let enterScope env = []::env
+let enter_scope env = []::env
 
 
 (* match_type: match type of right hand side to left hand side *)
@@ -35,13 +35,16 @@ let enterScope env = []::env
 let rec match_type t1 t2 =
 	match (t1,t2) with
 		| (TypeVoid,_)
-		| (TypeInt,_)
 		| (TypeBool,_)
+		| (TypeInt,_)
 		| (TypeString,_) -> if t1=t2 then true else false
 		| (TypeFloat,_) -> if ((t1=t2)||(t2=TypeInt)) then true else false
 		| (TypeUnion(a1,a2),TypeUnion(b1,b2)) -> if ((match_type a1 b1)&& (match_type a2 b2)) then true else false
 		| (TypeFunc(a1,_),TypeFunc(b1,_)) -> if (match_type a1 b1) then true else false
 		| (TypeArray(a1,i1),TypeArray(b1,i2)) -> if ((a1 = b1)&&(i1=i2)) then true else false
+		| (TypeClass(id1),TypeClass(id2)) -> if (id1=id2) then true else false
+		| (TypeClassDecl(id1,ext1),TypeClassDecl(id2,ext2)) -> if (id1=id2) then true else false
+		| (TypeClass(id1),TypeClassDecl(id2,ext)) -> if (id1=id2) then true else false
 		| (_,_) -> false
 									 
 
@@ -60,40 +63,85 @@ and get_decl_type env id =
 							match tmp with (value,decl) -> 
 							if value then decl else get_decl_type tail id
 
-
+(*get parameters*)
+and get_parameters lst =
+	match lst with
+	 | [] -> TypeVoid
+	 | [(id, pt)] -> pt
+	 | (id,pt)::tail -> TypeUnion(get_parameters tail, pt)
 (* apply f to all statements in a statement list *)
 and check_all_on_list f pl =
-match pl with
-[]	-> ()
-| h::t -> (f h;check_all_on_list f t)
+	match pl with
+	[]	-> ()
+	| head::tail -> (f head;check_all_on_list f tail)
 
 (* return a type of a lvalue *)
 and get_lvalue_type env l =
-match l with
-| Id i -> (get_decl_type env  i)
-| _ -> TypeInt
+	match l with
+	| Id i -> (get_decl_type env i)
+	| Member(e1,e2) as x-> let et1 = get_expr_type env e1 in
+						let et2 = get_expr_type env e2 in(
+						if (et2<>TypeInt) then raise (Type_Mismatch_In_Expression (Lhs(x)));
+							(match et1 with
+							| TypeArray(pt,_) -> pt
+							| _ -> raise (Type_Mismatch_In_Expression (Lhs(x)))
+							)
+						)
+	(* | Field(e,id) -> let et = get_expr_type env e in
+						if (et = (TypeClass "Self")) *)
+	| _ -> TypeInt
 
 (* return a type of a list of expression *)	
 and get_expr_list_type env el = 
-match el with
-[] -> TypeVoid
-| [e] -> let t = get_expr_type env e in t
-| h::t -> let ht = get_expr_type env h in 
-		  let tt = get_expr_list_type env t in TypeUnion (ht,tt)
+	match el with
+	[] -> TypeVoid
+	| [e] -> let t = get_expr_type env e in t
+	| head::tail -> let ht = get_expr_type env head in 
+			  let tt = get_expr_list_type env tail in TypeUnion (ht,tt)
 
 (* return a type of an expression *)
 and get_expr_type env e =
-match e with
-    Lit c -> get_const_type c
-  | Lhs l -> get_lvalue_type env l
-  | Unary (uop,e1) -> let et = get_expr_type env e1 in
+	match e with
+	| Self -> TypeClass "Self"
+    | Lit c -> get_const_type c
+  	| Lhs l -> get_lvalue_type env l
+  	| Unary (uop,e1) -> let et = get_expr_type env e1 in
 					(match uop with  
 					NotOp -> if et = TypeBool then TypeBool else raise (Type_Mismatch_In_Expression e)
 					| _ -> if (et = TypeInt || et = TypeFloat) then et else raise (Type_Mismatch_In_Expression e)
 					)
-
-	
-  | _ -> TypeInt
+	| Binary (biop,e1,e2) ->
+		let et1 = get_expr_type env e1 in
+		let et2 = get_expr_type env e2 in(
+		match biop with
+		|Plus |Minus |Mul -> if (((et1=TypeFloat)&&(match_type et1 et2))||
+									((et2=TypeFloat)&&(match_type et2 et1))) then TypeFloat 
+								else if ((et1=TypeInt)&&(match_type et1 et2)) then TypeInt
+										else raise (Type_Mismatch_In_Expression e)
+		|Div ->	if (((et1=TypeFloat)&&(match_type et1 et2))||
+					((et2=TypeFloat)&&(match_type et2 et1))||
+					((et1=TypeInt)&&(match_type et1 et2))) then TypeFloat 
+				else raise (Type_Mismatch_In_Expression e)
+		|IDiv | Mod -> if (et1=TypeInt && et1=et2) then TypeInt else raise (Type_Mismatch_In_Expression e)
+		| And | Or -> if (et1=TypeBool && et1=et2) then TypeBool else raise (Type_Mismatch_In_Expression e) 
+		| Eq | Neq -> if (et1=et2 && et1<>TypeVoid) then TypeBool else raise (Type_Mismatch_In_Expression e)
+		| Ge | Gt | Le | Lt -> if ((et1=TypeInt||et1=TypeFloat) && (et2=TypeInt || et2=TypeFloat))
+								then TypeBool else raise (Type_Mismatch_In_Expression e)
+		|Concat -> if (et1=et2 && et1=TypeString) then TypeString 
+					else raise (Type_Mismatch_In_Expression e)
+		)
+	|New(id,le) ->(
+		try(
+			let rt = get_decl_type env id in(
+			match rt with
+			| TypeClassDecl(id1,ext) -> TypeClassDecl(id1,ext)
+			| _ -> raise (Type_Mismatch_In_Expression e)
+			)
+		)	
+		with
+		|Undeclared_Identifier id -> raise (Undeclared_Class id)
+	)	
+	| _ -> TypeInt
   
 (* look up an identifier in a list *)
 and lookup id lst =
@@ -134,10 +182,10 @@ and add_decl env decl  =
 			if (lookup id head) 
 				then raise (Redeclared_Class id);
 			let env1 = add_new_id env id (TypeClassDecl(id, ext)) in
-			let env2 = enterScope env1 in (
+			let env2 = enter_scope env1 in (
 				try	(
 					let child = add_decl_list env2 dl in 
-					let hdchild = (List.hd child) in
+					let hdchild =(List.rev (List.hd child)) in
 					exitClass env1 hdchild
 				)
 				with
@@ -145,8 +193,11 @@ and add_decl env decl  =
 					| _ -> raise NeverHappen
 			)
 
+	(* 	| MethodProDecl(rt,id,dl) -> *)
+			
+
 		| MethodDecl(rt,sc,id,dl,(lc,sl)) ->
-			let env1 = enterScope env in
+			let env1 = enter_scope env in
 			let env2 = (
 				try add_decl_list env1 dl
 			 	with 
@@ -154,7 +205,8 @@ and add_decl env decl  =
 			) in 
 	 		let env3 = add_decl_list env2 lc in 
 	 			(check_all_on_list (check_decl_in_statement env3) sl;env)
-											
+
+
 		| _ -> env
     )
 
@@ -164,17 +216,20 @@ and add_new_id env id typ =
 		| [] -> raise NeverHappen
 		| head::tail -> ((id,typ)::head)::tail
 (* check a statement *)	
-and check_decl_in_statement env  st =
+and check_decl_in_statement env st =
 	match st with
-|IfThenElse (e,s1,s2) -> let t = get_expr_type env e in 
-						 if (t = TypeBool) 
-						 then ((check_decl_in_statement env s1);(check_decl_in_statement env s2)) 
-						 else raise (Type_Mismatch_In_Statement st)
-|Assign(l,e) -> let rt = get_expr_type env e in 
-				let lt = get_lvalue_type env l in 
-				if (not (match_type lt rt)) 
-				then raise (Type_Mismatch_In_Statement st)		 
-|_ -> ()
+	|Block(dl,sl) -> let env1 = add_decl_list (enter_scope env) dl in(
+						(check_all_on_list (check_decl_in_statement env1) sl)
+					)
+	|IfThenElse (e,s1,s2) -> let t = get_expr_type env e in 
+							 if (t = TypeBool) 
+							 then ((check_decl_in_statement env s1);(check_decl_in_statement env s2)) 
+							 else raise (Type_Mismatch_In_Statement st)
+	|Assign(l,e) -> let rt = get_expr_type env e in 
+					let lt = get_lvalue_type env l in 
+					if (not (match_type lt rt)) 
+					then raise (Type_Mismatch_In_Statement st)		 
+	|_ -> ()
 			
 (* add all declarations in a list into the enviroment *)
 and add_decl_list env  decl_list =
@@ -194,10 +249,7 @@ and add_items env name lst =
 and exitClass env lst = 
 		let hdEnv = List.hd env in
 		let (name,pt) = List.hd hdEnv in 
-  	add_items env name lst
-
-
-
+  			add_items env name lst
 				
 (* initializing the environment *)
 let initenv env =
@@ -219,18 +271,18 @@ let initenv env =
 					  
 let rec string_of_type pt = 
 match pt with
-| TypeInt 	-> "Int"
-| TypeFloat -> "Float"
-| TypeBool 	-> "Bool"
-| TypeString -> "String"
-| TypeArray (_,size) -> "Array " ^ "[" ^ (string_of_int size) ^"]"  
-| TypeUnion (t1,t2) -> "Union ("^(string_of_type t1)^","^(string_of_type t2)^")"  
-| TypeFunc (t1,t2) -> "Func ("^(string_of_type t1)^") => "^(string_of_type t2)
-| TypeMethod (t1,t2) -> "Method ("^(string_of_type t1)^") => "^(string_of_type t2)
-| TypeVoid -> "Void"
-| TypeClass name ->  "Class (" ^ name ^")"
-| TypeClassDecl (name,ext) ->  "ClassDecl (" ^ name ^ " : " ^ ext ^")"
-| TypeNull ->  "Null"
+	| TypeInt 	-> "Int"
+	| TypeFloat -> "Float"
+	| TypeBool 	-> "Bool"
+	| TypeString -> "String"
+	| TypeArray (_,size) -> "Array " ^ "[" ^ (string_of_int size) ^"]"  
+	| TypeUnion (t1,t2) -> "Union ("^(string_of_type t1)^","^(string_of_type t2)^")"  
+	| TypeFunc (t1,t2) -> "Func ("^(string_of_type t1)^") => "^(string_of_type t2)
+	| TypeMethod (t1,t2) -> "Method ("^(string_of_type t1)^") => "^(string_of_type t2)
+	| TypeVoid -> "Void"
+	| TypeClass name ->  "Class (" ^ name ^")"
+	| TypeClassDecl (name,ext) ->  "ClassDecl (" ^ name ^ " : " ^ ext ^")"
+	| TypeNull ->  "Null"
 		
 let ptbl env = 
 	Printf.printf "SCOPE LV 0\n\n";
@@ -246,5 +298,5 @@ let ptbl env =
 						  
 (* check whole program *)
 let check_program dl =
-	(*let env = add_decl_list (initenv [[]]) dl in ptbl env; ()*)
+(* 	let env = add_decl_list (initenv [[]]) dl in ptbl env; () *)
  	let _ = add_decl_list (initenv [[]]) dl in ()
